364c0182e350240f5da167f97c80c116b890ce8d
jeffmayn <jeff@gyldenbrand.dk>
2018-07-20 22:50:01 +0200

    ...

--- LN_EXAM/scripts/test.py
+++
@@ -1,9 +0,0 @@
-import sympy as sy
-import numpy as np
-
-A = np.array([[1,3,1,1],[2,-2,1,2],[3,1,2,-1]])
-
-b = np.array([[3],[8],[-1]])
-print np.linalg.matrix_rank(A)
-AA=np.column_stack([A,b])
-print sy.Matrix(AA).rref()
